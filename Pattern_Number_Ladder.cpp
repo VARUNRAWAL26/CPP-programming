4b7d9e5427cd034cc5f3de61ebaa9b8816b118b0
VARUNRAWAL26 <113203811+VARUNRAWAL26@users.noreply.github.com>
2022-09-11 12:54:21 +0530

    Create Pattern_Number_Ladder.cpp

---
+++ Pattern_Number_Ladder.cpp
@@ -0,0 +1,49 @@
+/*
+Take N (number of rows), print the following pattern (for N = 4)
+1
+2 3
+4 5 6
+7 8 9 10
+
+Input Format
+Constraints
+
+0 < N < 100
+Output Format
+
+Sample Input
+4
+
+Sample Output
+1  
+2	3  
+4	5	6  
+7	8	9	10
+
+Explanation
+Each number is separated from other by a tab.
+
+CODE
+*/
+#include<iostream>
+using namespace std;
+int main()
+{
+    int trows;
+    cin>>trows;
+    int row=1;
+    int c=1;
+    while(row<=trows)
+    {
+        int i=1;
+        while(i<=row)
+        {
+            cout<<c<<"\t";
+            c=c+1;
+            i=i+1;
+        }
+        row=row+1;
+        cout<<endl;
+    }
+    return 0;
+}
