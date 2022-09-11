bd99ef34a975d082b827fc6e976bfa94ad0c9835
VARUNRAWAL26 <113203811+VARUNRAWAL26@users.noreply.github.com>
2022-09-11 16:14:31 +0530

    Update Hollow_Diamond_Pattern(Pattern 6).cpp

--- Hollow_Diamond_Pattern(Pattern 6).cpp
+++ Hollow_Diamond_Pattern(Pattern 6).cpp
@@ -12,16 +12,17 @@ Constraints
 
 0 < N < 10 (where N is an odd number)
 Output Format
+* * * * *
+* *   * *
+*       *
+* *   * *
+* * * * *
 
 Sample Input
 5
 
 Sample Output
-* *	* * *		  
-* *	  * *
-* 	    *
-* *   * *
-* *	* * *	
+
 
 Explanation
 Each '*' is separated from other by a tab.
